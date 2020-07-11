8cf1c69b77469a8c3d660699764c39ebd3539174
aadityaiiitu@gmail.com <aadityaiiitu@gmail.com>
2020-07-11 06:14:17 +0530

    removed minor bugs and now making create tours function

--- controllers/tourControllers.js
+++ controllers/tourControllers.js
@@ -16,15 +16,15 @@ const router = require('../Routes/tourRouter');
 //   next();
 // };
 
-exports.checkBody = (req, res, next) => {
-  if (!req.body.name || !req.body.price) {
-    return res.status(400).json({
-      status: 'fail',
-      message: 'Bad Request price or name is not avail',
-    });
-  }
-  next();
-};
+// exports.checkBody = (req, res, next) => {
+//   if (!req.body.name || !req.body.price) {
+//     return res.status(400).json({
+//       status: 'fail',
+//       message: 'Bad Request price or name is not avail',
+//     });
+//   }
+//   next();
+// };
 
 exports.getAllTours = (req, res) => {
   res.status(200).json({
@@ -38,8 +38,8 @@ exports.getAllTours = (req, res) => {
 };
 
 exports.getTour = (req, res) => {
-  const id = req.params.id * 1;
-  console.log(req.params);
+  //   const id = req.params.id * 1;
+  //   console.log(req.params);
   // const tour = tours.find((el) => el.id === id);
   // if (!tour) {
   //   return res.status(404).json({
@@ -49,49 +49,49 @@ exports.getTour = (req, res) => {
   // }
   res.status(200).json({
     status: 'success',
-    data: {
-      // tour,
-    },
+    //   data: {
+    // tour,
+    //     },
   });
 };
 
-// exports.createTour = (req, res) => {
-//   const newId = tours[tours.length - 1].id + 1;
-//   const newTour = Object.assign({ id: newId }, req.body);
+exports.createTour = (req, res) => {
+  //   const newId = tours[tours.length - 1].id + 1;
+  //   const newTour = Object.assign({ id: newId }, req.body);
 
-//   tours.push(newTour);
-//   fs.writeFile(
-//     `${__dirname}/dev-data/data/tours-simple.json`,
-//     JSON.stringify(tours),
-//     (err) => {
-//       res.status(201).json({
-//         status: 'success',
-//         data: {
-//           tour: newTour,
-//         },
-//       });
-//     }
+  //   tours.push(newTour);
+  //   fs.writeFile(
+  //     `${__dirname}/dev-data/data/tours-simple.json`,
+  //     JSON.stringify(tours),
+  //     (err) => {
+  res.status(201).json({
+    status: 'success',
+    //         data: {
+    //           tour: newTour,
+    //         },
+  });
+};
 //   );
 // };
 
 exports.updateTour = (req, res) => {
   res.status(200).json({
     status: 'success',
-    data: {
-      // tour: '<Placeholder data for now>',
-    },
+    // data: {
+    // tour: '<Placeholder data for now>',
+    // },
   });
 };
 
-// exports.deleteTour = (req, res) => {
-//   if (req.params.id * 1 > tours.length) {
-//     res.status(404).json({
-//       status: 'fail',
-//       message: 'Invalid Id',
-//     });
-//   }
-  // res.status(200).json({
-    // status: 'success',
-    // data: null,
-//   });
-// };
+exports.deleteTour = (req, res) => {
+  //   if (req.params.id * 1 > tours.length) {
+  // res.status(404).json({
+  //   status: 'fail',
+  //   message: 'Invalid Id',
+  // });
+  //   }
+  res.status(200).json({
+    status: 'success',
+    data: null,
+  });
+};
