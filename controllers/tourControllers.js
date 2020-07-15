e773a00505394a06a7622bd8d61a9b5a5136eb36
aadityaiiitu@gmail.com <aadityaiiitu@gmail.com>
2020-07-15 13:07:26 +0530

    implemented the sorting and fields functionality

--- controllers/tourControllers.js
+++ controllers/tourControllers.js
@@ -33,7 +33,7 @@ exports.getAllTours = async (req, res) => {
 
     // Build a query
     const queryObj = { ...req.query };
-    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+    const excludedFields = ['page', 'sort', 'limit'];
     excludedFields.forEach((el) => delete queryObj[el]);
 
     //advance filtering
@@ -41,7 +41,26 @@ exports.getAllTours = async (req, res) => {
 
     queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
 
-    const query = Tour.find(JSON.parse(queryStr));
+    console.log(JSON.parse(queryStr));
+
+    let query = Tour.find(JSON.parse(queryStr));
+
+    //Sorting
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(',').join(' ');
+      console.log(sortBy);
+      query = query.sort(sortBy);
+    } else {
+      query = query.sort(-createdAt);
+    }
+
+    //Limit fields
+    if (req.query.fields) {
+      const fields = req.query.fields.split(',').join(' ');
+      query = query.select(fields);
+    } else {
+      query = query.select('-__v');
+    }
 
     // exicute query
     const tours = await query;
