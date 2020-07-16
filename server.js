8db274d0d456c51d26b153fff57563b2fe045b9c
aadityaiiitu@gmail.com <aadityaiiitu@gmail.com>
2020-07-16 19:55:11 +0530

    added global error handelers and the validation errors and set two modes for errors prod and dev

--- server.js
+++ server.js
@@ -2,6 +2,12 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const app = require('./app');
 
+process.on('uncaughtException', (err) => {
+  console.log('UNCAUGHT EXEPTION, shutting down');
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
 dotenv.config({ path: './config.env' });
 
 const DB = process.env.DATABASE.replace(
@@ -17,6 +23,7 @@ mongoose
     useUnifiedTopology: true,
   })
   .then(() => console.log('connection successful!!!'));
+// .catch((err) => console.log('ERROR'));
 
 // const testTour = new Tour({
 //   name: 'The Park Camper',
@@ -33,6 +40,14 @@ mongoose
 //   });
 
 const PORT = process.env.PORT || 4000;
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(` app running on port no ${PORT}....`);
+});
+
+process.on('unhandledRejection', (err) => {
+  console.log('UNHANDELED REJECTION. shutting down');
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
 });
