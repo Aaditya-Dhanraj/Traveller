0888db8bc3a6b87202cbd7658b72b6909c709c3f
aadityaiiitu@gmail.com <aadityaiiitu@gmail.com>
2020-08-10 22:20:40 +0530

    last few changes

--- controllers/bookingControllers.js
+++ controllers/bookingControllers.js
@@ -19,7 +19,9 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
     //   req.params.tourId
     // }&user=${req.user.id}&price=${tour.price}`,
 
-    success_url: `${req.protocol}://${req.get('host')}/my-tours?alert=booking`,
+    success_url: `${req.protocol}://${req.get('host')}/my-tours/?tour=${
+      zreq.params.tourId
+    }&user=${req.user.id}&price=${tour.price}`,
 
     cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
 
